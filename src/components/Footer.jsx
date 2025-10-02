21beb816814e60f5674bd724c1756390a64411b1
Muzammil Shaikh <muzammilggshaikh@gmail.com>
2025-10-03 00:54:54 +0530

    Updating Footer Code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,6 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 text-sm text-white flex items-center justify-between">
         <div className="text-left">
           <p>© 2025 Pocket Campus</p>
-          {/* <p className="mt-0.5">
-            Check out the source code on{" "}
-            <a
-              href="https://github.com/YourGitHubUsername/Pocket-Campus"
-              className="text-gray-300 underline hover:text-white transition-colors duration-200"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </p> */}
         </div>
         <p className="text-right">
           Developed by <span className="font-semibold">Muzammil Shaikh</span>.
